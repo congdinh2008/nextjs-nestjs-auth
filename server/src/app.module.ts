29d585b1a6d8545d16b8a6d2b8e770a69239090d
Cong Dinh <congdinhvannguyen165@outlook.com>
2022-10-23 16:08:22 +0700

    Config MongoDB Connection in App.Module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
 import { getEnvPath } from './helpers/env.helper';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 const envFilePath: string = getEnvPath(`${__dirname}/environments`);
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: envFilePath, isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: envFilePath, isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        dbName: configService.get<string>('MONGODB_DBNAME'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
