e011bb2c97eeeb9a8034a178ecee3d7f56bc0dff
Cong Dinh <congdinhvannguyen165@outlook.com>
2022-10-23 16:31:42 +0700

    Server: Config Swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,24 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //#region ConfigSwagger
+  const config = new DocumentBuilder()
+    .setTitle('NestJS JWT Authentication')
+    .setDescription('NestJS JWT Authentication')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //#endregion
+
   const PORT = app.get(ConfigService).get('PORT');
   await app.listen(PORT, () => {
     console.log(`Service listening on port ${PORT}`);
